705eb2644ac187d4b3375e19bd9e44dea87e0ab3
IshwarVaidya2531 <141947486+IshwarVaidya2531@users.noreply.github.com>
2024-05-27 12:42:24 +0530

    First commit

---
+++ ClothingSystem/src/main/java/com/example/ClothingSystem/ClothingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClothingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingSystemApplication.class, args);
+	}
+
+}
